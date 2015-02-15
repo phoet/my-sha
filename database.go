74a95df92db02bbb1a2051be3a14636b9427b59b
phoet <phoetmail@googlemail.com>
2015-02-15 18:06:32 +0100

    have it create repos

--- database.go
+++ database.go
@@ -10,102 +10,65 @@ import (
 )
 
 func main() {
-	// initialize the DbMap
 	dbmap := initDb()
 	defer dbmap.Db.Close()
 
-	// delete any existing rows
 	err := dbmap.TruncateTables()
 	checkErr(err, "TruncateTables failed")
 
-	// create two posts
-	p1 := newPost("Go 1.1 released!", "Lorem ipsum lorem ipsum")
-	p2 := newPost("Go 1.2 released!", "Lorem ipsum lorem ipsum")
+	repo := newRepo("app123")
 
-	// insert rows - auto increment PKs will be set properly after the insert
-	err = dbmap.Insert(&p1, &p2)
+	err = dbmap.Insert(&repo)
 	checkErr(err, "Insert failed")
 
-	// use convenience SelectInt
-	count, err := dbmap.SelectInt("select count(*) from posts")
+	count, err := dbmap.SelectInt("select count(*) from repos")
 	checkErr(err, "select count(*) failed")
 	log.Println("Rows after inserting:", count)
 
-	// update a row
-	p2.Title = "Go 1.2 is better than ever"
-	count, err = dbmap.Update(&p2)
+	repo.Revision = "AABBCCDD"
+	count, err = dbmap.Update(&repo)
 	checkErr(err, "Update failed")
 	log.Println("Rows updated:", count)
 
-	// fetch one row - note use of "post_id" instead of "Id" since column is aliased
-	//
-	// Postgres users should use $1 instead of ? placeholders
-	// See 'Known Issues' below
-	//
-	err = dbmap.SelectOne(&p2, "select * from posts where post_id=$1", p2.Id)
+	err = dbmap.SelectOne(&repo, "select * from repos where id=$1", repo.Id)
 	checkErr(err, "SelectOne failed")
-	log.Println("p2 row:", p2)
+	log.Println("repo row:", repo)
 
-	// fetch all rows
-	var posts []Post
-	_, err = dbmap.Select(&posts, "select * from posts order by post_id")
+	var repos []Repo
+	_, err = dbmap.Select(&repos, "select * from repos")
 	checkErr(err, "Select failed")
 	log.Println("All rows:")
-	for x, p := range posts {
-		log.Printf("    %d: %v\n", x, p)
+	for x, r := range repos {
+		log.Printf("    %d: %v\n", x, r)
 	}
-
-	// delete row by PK
-	count, err = dbmap.Delete(&p1)
-	checkErr(err, "Delete failed")
-	log.Println("Rows deleted:", count)
-
-	// delete row manually via Exec
-	_, err = dbmap.Exec("delete from posts where post_id=$1", p2.Id)
-	checkErr(err, "Exec failed")
-
-	// confirm count is zero
-	count, err = dbmap.SelectInt("select count(*) from posts")
-	checkErr(err, "select count(*) failed")
-	log.Println("Row count - should be zero:", count)
 
 	log.Println("Done!")
 }
 
-type Post struct {
-	// db tag lets you specify the column name if it differs from the struct field
-	Id      int64 `db:"post_id"`
-	Created int64
-	Title   string
-	Body    string
+type Repo struct {
+	Id       int64 `db:"id"`
+	App      string
+	Revision string
+	Token    string
+	Created  int64
 }
 
-func newPost(title, body string) Post {
-	return Post{
+func newRepo(app string) Repo {
+	return Repo{
 		Created: time.Now().UnixNano(),
-		Title:   title,
-		Body:    body,
+		App:     app,
 	}
 }
 
 func initDb() *gorp.DbMap {
-	// connect to db using standard Go database/sql API
-	// use whatever database/sql driver you wish
-	// db, err := sql.Open("sqlite3", "/tmp/post_db.bin")
-	// db, err := sql.Open("postgres", "postgres://pqgotest:password@localhost/pqgotest?sslmode=verify-full")
-	db, err := sql.Open("postgres", "postgres://postgres@localhost/my_sha-development?sslmode=disable")
+	url := mustGetenv("DATABASE_URL")
+	db, err := sql.Open("postgres", url)
 	checkErr(err, "sql.Open failed")
 
-	// construct a gorp DbMap
-	// dbmap := &gorp.DbMap{Db: db, Dialect: gorp.SqliteDialect{}}
 	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
 
-	// add a table, setting the table name to 'posts' and
-	// specifying that the Id property is an auto incrementing PK
-	dbmap.AddTableWithName(Post{}, "posts").SetKeys(true, "Id")
+	dbmap.AddTableWithName(Repo{}, "repos").SetKeys(true, "Id")
 
-	// create the table. in a production system you'd generally
-	// use a migration tool, or create the tables via scripts
 	err = dbmap.CreateTablesIfNotExists()
 	checkErr(err, "Create tables failed")
 
